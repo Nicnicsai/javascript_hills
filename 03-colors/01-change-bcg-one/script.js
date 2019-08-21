57b3fb0d804ae130833becde7dea0c0ffcd1c1b6
Your Name <you@example.com>
2019-08-21 15:02:22 +0200

    change bg color

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -11,6 +11,30 @@
 
 (function() {
 
-    // your code here
+
+
+    document.getElementById("red").addEventListener("click", function(){
+        document.body.style.background = "red";
+    });
+
+    document.getElementById("green").addEventListener("click", function(){
+        document.body.style.background = "green";
+    });
+
+    document.getElementById("yellow").addEventListener("click", function(){
+        document.body.style.background = "yellow";
+    });
+
+    document.getElementById("blue").addEventListener("click", function(){
+        document.body.style.background = "blue";
+    });
+
+
+
+
 
 })();
+
+
+
+
