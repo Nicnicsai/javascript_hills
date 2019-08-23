9e76a048ac522c2bc66427a8a7457671dbbdc2bb
Your Name <you@example.com>
2019-08-23 11:48:53 +0200

    easy exercice arrays 2

--- 05-arrays/02-manipulate-array/script.js
+++ 05-arrays/02-manipulate-array/script.js
@@ -24,6 +24,14 @@
         "cerise",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener('click', function () {
+
+        fruits[0] = "banana";
+        fruits[9] = "kiwi";
+
+        alert(fruits);
+
+    });
+
 
 })();
