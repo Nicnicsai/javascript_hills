28a68e4fc82800ce6be43c1602d5ef518f492934
Your Name <you@example.com>
2019-08-23 11:17:04 +0200

    4.5 fixed

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -1,16 +1,33 @@
-/* becode/javascript
- *
- * /04-dates/05-get-spooky-fridays/script.js - 4.5: calcul des vendredi 13
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+    var input_year = document.getElementById("year").value; //input year
+    var d = new Date();
 
-    // your code here
+
+    d.setFullYear(input_year, 0, 13);
+    //console.log(d);
+
+    var current_month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    var res_months = []; //empty array
+
+    for (var m = 0; m < 12; m++) {
+        //set the date again with m
+        d.setMonth(m);
+        // console.log(d);
+
+        // ask if it is friday
+
+        if (d.getDay() === 5) {
+            //get the month(-name)
+
+            current_month_name[d.getMonth()];
+
+            res_months.push(current_month_name[d.getMonth()]);
+        }
+    }
+
+    alert(res_months);
 
 })();
