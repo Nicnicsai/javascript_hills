a09f8b0b0fc017956b7134ef8af2141b6f3271c9
Your Name <you@example.com>
2019-08-28 19:17:43 +0200

    hover over image

--- 06-dom/04-attr-create/script.js
+++ 06-dom/04-attr-create/script.js
@@ -11,6 +11,22 @@
 
 (function() {
 
-    // your code here
+    // get value of data-image attribute from element w/ id source
+
+    let value = document.querySelector("#source").getAttribute("data-image");
+
+    // create new image tag inside target element. w/ value from data-image.
+
+    var img = document.createElement("IMG"); // creates new tag element
+    //var node = document.createTextNode(value); // creates text node first
+    //img.appendChild(node); //append text node to <img> element
+    var element = document.getElementById("target"); // Finally you must append the new element to an existing element
+    //This code finds an existing element
+    element.appendChild(img); //This code appends the new element to the existing element
+    img.src = value;
+
+
+    // remove first element
+    document.getElementbyId("source").removeAttribute("data-image");
 
 })();
--- 06-dom/05-hover-image/script.js
+++ 06-dom/05-hover-image/script.js
@@ -1,16 +1,34 @@
-/* becode/javascript
- *
- * /06-dom/05-hover-image/script.js - 6.5: survol d'image
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+
+    //var imgSource = document.getElementsByTagName("img")[0].src; //img src link ==> file:...
+
+    var imgSrc = document.getElementsByTagName("img")[0].getAttribute("src"); //img src link only!
+    var hoverLink = document.getElementsByTagName("img")[0].getAttribute("data-hover"); //data-hover value
+
+
+    var imgSource = document.getElementsByTagName("img")[0]; // whole image element (bona code)
+
+
+    //console.log(imgSource.value); ??undefined (ask Bona about her code)
+
+    // https://www.w3schools.com/js/js_htmldom_eventlistener.asp and https://www.w3schools.com/jsref/dom_obj_event.asp
+
+    // when hovering over image
+imgSource.addEventListener("mouseover", function() {
+
+    imgSource.setAttribute("src", hoverLink);
+
+});
+
+// when not hovering over image
+imgSource.addEventListener("mouseout", function() {
+
+    imgSource.setAttribute("src", imgSrc);
+
+});
+
 
 })();
