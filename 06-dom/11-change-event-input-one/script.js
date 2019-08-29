b2febf468e618d28601054962741ddaabb9b5513
Your Name <you@example.com>
2019-08-29 14:28:30 +0200

    change event 6.11

--- 06-dom/11-change-event-input-one/script.js
+++ 06-dom/11-change-event-input-one/script.js
@@ -1,16 +1,30 @@
-/* becode/javascript
- *
- * /06-dom/11-change-event-input-one/script.js - 6.11: événement change (1)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+
+    //add event listener
+
+    document.getElementById("pass-one").addEventListener("input", function() {
+
+
+        var passOne = document.getElementById("pass-one").value;
+
+
+        console.log(passOne);
+        var numberL = passOne.length; // count number of characters of value
+
+
+        document.getElementById("pass-one").maxLength = 10; //max length characters
+
+        document.getElementById("counter").innerHTML = numberL + "/10";
+
+
+    })
+
+
+
+
+
 
 })();
