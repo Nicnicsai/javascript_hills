c57b38528ffd3c16521087dbda27de4bb9312e11
Your Name <you@example.com>
2019-08-30 11:13:48 +0200

    wave exercice

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -15,44 +15,74 @@
 
     var wave = target.innerHTML = "Kaaaabbbbbbbooooooooommmmmiiiiiiiiiiiiiiiiiiiiiii!";
 
-    document.getElementById("target").innerHTML = "";
+    target.innerHTML = "";
 
     var pos = 0; //counter
 
 
+
+
     for (var i=0; i < wave.length ; i++) {  //define the i with var/let
+
 
         var char = wave.charAt(i);
         pos++; // need two counters
 
-        if ( pos == 0){
+        if ( pos == 1){
 
             char = "<span class='1'>" + char + "</span>";
 
-        } if ( pos == 1  || pos == 7) {
+        } if ( pos == 2  || pos == 8) {
 
             char = "<span class='2'>" + char + "</span>";
 
-        } if ( pos == 2 || pos == 6) {
+        } if ( pos == 3 || pos == 7) {
 
             char = "<span class='3'>" + char + "</span>";
 
-        } if ( pos == 3 || pos == 5 ) {
+        } if ( pos == 4 || pos == 6 ) {
 
             char = "<span class='4'>" + char + "</span>";
 
-        } if (pos == 4) {
+        } if (pos == 5) {
 
             char = "<span class='5'>" + char + "</span>";
 
-        } if (pos > 7) {
+        } if (pos > 7) { //when it arrives at the last one > assign 0 to position
 
-            pos = 0;
+            pos = 0; // 1 '=' to assign a value to it. ('==' compare it)
         }
 
+        target.innerHTML += char;
 
     }
 
+    var fs = document.getElementsByClassName('1');
+
+    for (var y = 0; y < fs.length; y++) {
+        fs[y].style.fontSize = '6px';
+    }
+
+    var fss = document.getElementsByClassName('2');
+
+    for (var z = 0; z < fss.length; z++) {
+        fss[z].style.fontSize = '12px';
+    }
+
+    var fsss = document.getElementsByClassName('3');
+
+    for (var u = 0; z < fsss.length; u++) {
+        fsss[u].style.fontSize = '20px';
+    }
+
+
+// nog 4 en 5
+
+
+
+    /*document.getElementsByClassName("1")[0].style.fontSize("6px");
+    document.getElementsByClassName("2")[0].style.fontSize("10px");
+    document.getElementsByClassName("3")[0].style.fontSize("20px");*/
 
 
 })();
--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -4,17 +4,9 @@
 
     var output = document.getElementById("target");
 
-
-    var button_1 = document.getElementById("part-one");
-    var button_2 = document.getElementById("part-two");
-    var button_3 = document.getElementById("part-three");
-    var button_4 = document.getElementById("part-four");
-
-    console.log(button_1);
-
     var buttons  = document.getElementsByTagName("button");
 
-
+    console.log(buttons);
 
 
 
@@ -32,4 +24,10 @@ var b = Array.prototype.slice.call(document.getElementById("part-two"));
 var c = Array.prototype.slice.call(document.getElementById("part-three"));
 var d = Array.prototype.slice.call(document.getElementById("part-four"));
 var buttons = a.concat(b,c,d);
-console.log(buttons);*/
+console.log(buttons);
+
+var button_1 = document.getElementById("part-one");
+var button_2 = document.getElementById("part-two");
+var button_3 = document.getElementById("part-three");
+var button_4 = document.getElementById("part-four");
+*/
