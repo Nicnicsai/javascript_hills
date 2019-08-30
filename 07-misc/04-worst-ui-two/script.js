afc1feb4940a89092e2849bbed8469aaecbd2e3b
Your Name <you@example.com>
2019-08-30 09:40:48 +0200

    slider 2

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -1,16 +1,35 @@
-/* becode/javascript
- *
- * /07-misc/04-worst-ui-two/script.js - 7.4: la pire interface (2) : phone clicker
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
 
-    // your code here
+
+
+    var output = document.getElementById("target");
+
+
+    var button_1 = document.getElementById("part-one");
+    var button_2 = document.getElementById("part-two");
+    var button_3 = document.getElementById("part-three");
+    var button_4 = document.getElementById("part-four");
+
+    console.log(button_1);
+
+    var buttons  = document.getElementsByTagName("button");
+
+
+
+
+
+
+
+
+
 
 })();
+
+
+/*
+var a = Array.prototype.slice.call(document.getElementById("part-one"));
+var b = Array.prototype.slice.call(document.getElementById("part-two"));
+var c = Array.prototype.slice.call(document.getElementById("part-three"));
+var d = Array.prototype.slice.call(document.getElementById("part-four"));
+var buttons = a.concat(b,c,d);
+console.log(buttons);*/
