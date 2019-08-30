7ca04344031891042602998afa2903e62d8d4bd2
Your Name <you@example.com>
2019-08-30 08:56:04 +0200

    slider 1

--- 07-misc/02-typewriter-effect/script.js
+++ 07-misc/02-typewriter-effect/script.js
@@ -1,16 +1,32 @@
-/* becode/javascript
- *
- * /07-misc/02-typewriter-effect/script.js - 7.2: effet machine à écrire
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+(function () {
+
+    var txt = document.getElementById("target").innerHTML;
+    var i = 0;
+    var speed = 50; //speed duration
+
+    document.getElementById("target").innerHTML = "";
+
+    console.log(txt);
+
+
+
+        function typeWriter() {
+
+            if (i < txt.length) {
+                document.getElementById("target").innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+
+        }
+
+        typeWriter()
+
 
 })();
+
+
+//look code again
--- 07-misc/03-worst-ui-one/script.js
+++ 07-misc/03-worst-ui-one/script.js
@@ -1,16 +1,21 @@
-/* becode/javascript
- *
- * /07-misc/03-worst-ui-one/script.js - 7.3: la pire interface (1) : phone slider
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
     // your code here
+
+/*var minValue = document.getElementById("slider").min; //0460000000
+var maxValue = document.getElementById("slider").max; //0499999999*/
+
+
+var slider = document.getElementById("slider");
+var output = document.getElementById("target");
+
+
+    output.innerHTML = "0" + slider.value;
+    slider.oninput = function() {
+        output.innerHTML = "0" + this.value;
+    }
+
+
 
 })();
