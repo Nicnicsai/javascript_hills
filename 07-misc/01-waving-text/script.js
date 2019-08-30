d309b956845ca3b4a65a30e5ef48b0de47a5a7a8
Your Name <you@example.com>
2019-08-30 10:28:28 +0200

    wave

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -9,8 +9,50 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    var target = document.getElementById("target");
+
+    var wave = target.innerHTML = "Kaaaabbbbbbbooooooooommmmmiiiiiiiiiiiiiiiiiiiiiii!";
+
+    document.getElementById("target").innerHTML = "";
+
+    var pos = 0; //counter
+
+
+    for (var i=0; i < wave.length ; i++) {  //define the i with var/let
+
+        var char = wave.charAt(i);
+        pos++; // need two counters
+
+        if ( pos == 0){
+
+            char = "<span class='1'>" + char + "</span>";
+
+        } if ( pos == 1  || pos == 7) {
+
+            char = "<span class='2'>" + char + "</span>";
+
+        } if ( pos == 2 || pos == 6) {
+
+            char = "<span class='3'>" + char + "</span>";
+
+        } if ( pos == 3 || pos == 5 ) {
+
+            char = "<span class='4'>" + char + "</span>";
+
+        } if (pos == 4) {
+
+            char = "<span class='5'>" + char + "</span>";
+
+        } if (pos > 7) {
+
+            pos = 0;
+        }
+
+
+    }
+
+
 
 })();
