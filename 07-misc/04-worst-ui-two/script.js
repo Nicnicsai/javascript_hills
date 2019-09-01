4fa33e7190b69bced02ecfb8ec6e2a9c7568b43a
Your Name <you@example.com>
2019-09-01 11:31:15 +0200

    7.4

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -2,6 +2,35 @@
 
 
 
+// make array from buttons + add for each
+
+    Array.from(document.getElementsByTagName("button")).forEach(function(element) {
+
+// for each button add eventlistener
+
+        element.addEventListener("click", function() {
+
+            //element is already in buttons, so only need to getAttribute
+            var min = +(element.getAttribute("data-min"));
+            var max = +(element.getAttribute("data-max"));
+            var current = +(element.innerHTML);
+
+            //console.log(current);
+
+            if (current <= max) {
+                current++;
+                element.innerHTML = current;
+            } else {
+                element.innerHTML = min;
+            } if (current < 10) {
+                element.innerHTML = "0" + current; //leading 0
+            }
+
+            document.getElementById("target").innerHTML = "+" +
+                document.getElementById("part-one").innerHTML +
+                document.getElementById("part-two").innerHTML +
+                document.getElementById("part-three").innerHTML +
+                document.getElementById("part-four").innerHTML;
 
 
 
@@ -9,20 +38,8 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        });
+    });
 
 })();
 
