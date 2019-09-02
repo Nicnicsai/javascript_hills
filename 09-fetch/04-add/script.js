14b56d1c3a57580d06548eec7787db0993d1a544
Your Name <you@example.com>
2019-09-02 17:43:20 +0200

    new array

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -10,5 +10,55 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+
+        fetch('http://localhost:63342/becode_projects/javascript_hills/_shared/api.json')   //check which is the portname
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+
+
+               /* var name = data.heroes[i].name;
+                var alterEgo = data.heroes[i].alterEgo;
+                var powers = data.heroes[i].abilities;*/
+
+
+             //get info from input
+
+                var newName = document.getElementById("hero-name").value;
+                var newAlterEgo = document.getElementById("hero-alter-ego").value;
+                var newPowers = document.getElementById("hero-powers").value;
+
+
+                //make array from information
+
+                let newHero = {
+                    name: newName,
+                    alterego: newAlterEgo,
+                    abilities: newPowers
+                };
+
+
+                data.heroes.push(newHero);
+
+                console.log(data.heroes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            })
+
+
+    })
 })();
