a21841044fcfd68f1e4d987ef361d0e8a1979878
Your Name <you@example.com>
2019-09-02 16:49:41 +0200

    details

--- 09-fetch/03-details/script.js
+++ 09-fetch/03-details/script.js
@@ -10,5 +10,65 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    document.getElementById("run").addEventListener("click", function () {
+
+        fetch('http://localhost:63342/becode_projects/javascript_hills/_shared/api.json')   //check which is the portname
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+
+                // get ID (value) from form
+
+                var value = document.getElementById("hero-id").value; // 1 to 5
+
+                function findHero(hero){
+                    return hero.id == value; // found hero with ID.. == string and number so not ===
+
+                }
+
+               var heroInfo = data.heroes.find(findHero);
+
+                var temp = document.getElementsByTagName("template")[0];
+
+
+                //get info from array and put in innerHTML
+
+                    var name = data.heroes[value].name;
+
+                    var alterEgo = data.heroes[value].alterEgo;
+
+                    var powers = data.heroes[value].abilities;
+
+
+
+                    //show template
+
+                    var clon = temp.content.cloneNode(true); //clone floats around in memory
+
+                    //console.log(clon);
+                    // assign values to this clone
+
+                    clon.querySelector(".name").innerHTML = name;
+                    clon.querySelector(".alter-ego").innerHTML = alterEgo;
+                    clon.querySelector(".powers").innerHTML = powers;
+
+                    // here append clone and therefore becomes element
+
+                    document.getElementById("target").appendChild(clon); // template cloned and put here
+
+
+
+
+            })
+
+
+    })
+
+
+
+
+
 })();
