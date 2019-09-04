fe702117a88a471a97b2bac68b3404505b4adc4c
Your Name <you@example.com>
2019-09-04 15:32:00 +0200

    update game

--- js-games/game_1_rock/game_1.js
+++ js-games/game_1_rock/game_1.js
@@ -1,3 +1,4 @@
+
 (function () {
 
     //random choice computer
@@ -19,30 +20,57 @@
     //console.log(buttons);
 
     document.querySelectorAll('.buttons').forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
 
-            console.log(indexOf(button));
+            var chosenButton = button.getAttribute("id");
 
-
+            if (chosenButton === "scissors-btn") {
+                imgSourcePlayer.setAttribute("src", gallery[0]);
+            } else if (chosenButton === "rock-btn") {
+                imgSourcePlayer.setAttribute("src", gallery[1]);
+            } else {
+                imgSourcePlayer.setAttribute("src", gallery[2]);
+            }
 
         });
     });
 
 
-
-
-
     // click PLAY button
 
-    document.getElementById("play-btn").addEventListener("click", function() {
+    document.getElementById("play-btn").addEventListener("click", function () {
+
 
         // var indexGallery = gallery.indexOf(firstOne);  //index of..
 
-        imgSourceComputer.setAttribute("src",gallery[Math.floor(Math.random() * 3)]);
+        imgSourceComputer.setAttribute("src", gallery[Math.floor(Math.random() * 3)]);
+
+        var player = imgSourcePlayer.getAttribute("src");
+        var computer = imgSourceComputer.getAttribute("src");
+
+        if ((player === "assets/scissors.png" && computer === "assets/paper.png")
+            || (player === "assets/rock.png" && computer === "assets/scissors.png")
+            || (player === "assets/paper.png" && computer === "assets/rock.png")) {
+            document.getElementById("winner-result").innerHTML = "Winner: YOU!!!";
+
+            confetti.start();
+
+
+
+        } else if ((player === "assets/scissors.png" && computer === "assets/rock.png")
+            || (player === "assets/rock.png" && computer === "assets/paper.png")
+            || (player === "assets/paper.png" && computer === "assets/scissors.png")) {
+            document.getElementById("winner-result").innerHTML = "Winner: Evil Computer";
+
+        } else {
+            document.getElementById("winner-result").innerHTML = "Winner: EVEN!";
+        }
+
+
     });
 
 
 
 
-
 })();
+
