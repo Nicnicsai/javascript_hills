c35255a4ce3af07ed2aff93868763a7c8217dcf5
Nic Reyes Guttmann <53224395+Nicnicsai@users.noreply.github.com>
2020-01-17 09:50:36 +0100

    Update script.js

--- 06-dom/05-hover-image/script.js
+++ 06-dom/05-hover-image/script.js
@@ -1,34 +1,18 @@
 (function () {
 
 
+    let img = document.querySelector("figure img");
+    let imgSrc = img.src;
+    let imgHover = img.getAttribute("data-hover");
 
+    img.addEventListener("mouseenter", function(){
+        img.setAttribute("src", imgHover);
+    });
 
-    //var imgSource = document.getElementsByTagName("img")[0].src; //img src link ==> file:...
-
-    var imgSrc = document.getElementsByTagName("img")[0].getAttribute("src"); //img src link only!
-    var hoverLink = document.getElementsByTagName("img")[0].getAttribute("data-hover"); //data-hover value
-
-
-    var imgSource = document.getElementsByTagName("img")[0]; // whole image element (bona code)
-
-
-    //console.log(imgSource.value); ??undefined (ask Bona about her code)
-
-    // https://www.w3schools.com/js/js_htmldom_eventlistener.asp and https://www.w3schools.com/jsref/dom_obj_event.asp
-
-    // when hovering over image
-imgSource.addEventListener("mouseover", function() {
-
-    imgSource.setAttribute("src", hoverLink);
-
-});
-
-// when not hovering over image
-imgSource.addEventListener("mouseout", function() {
-
-    imgSource.setAttribute("src", imgSrc);
-
-});
-
+    img.addEventListener("mouseout", function(){
+        img.setAttribute("src", imgSrc);
+    })
+    
+    
 
 })();
